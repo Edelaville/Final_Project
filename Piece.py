c0ccdd18fecfe8579ab56bd74317d6fc2a9421c8
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-12-07 12:31:19 -0500

    Update Piece.py

--- Piece.py
+++ Piece.py
@@ -1,1 +1,31 @@
+class pieces:
+    def __init__(self, type):
+        piece_types = ["dog", "hat", "car", "ship", "wheelbarrow", "shoe"]
+        self.valid = False
+        for x in piece_types:
+            if type == x:
+                self.valid = True
+        self.type = type
+        self.position = [0, 0]
+        self.row_coef = 0
+        self.col_coef = 1
+    
+    def move(self):
+        if self.position[0] == 0 and self.position[1] == 0:
+            self.row_coef = 0
+            self.col_coef = 1
+            
+        if self.position[0] == 0 and self.position[1] == 9:
+            self.row_coef = 1
+            self.col_coef = 0
 
+        if self.position[0] == 9 and self.position[1] == 9:
+            self.row_coef = 0
+            self.col_coef = -1
+            
+        if self.position[0] == 9 and self.position[1] == 0:
+            self.row_coef = -1
+            self.col_coef = 0
+        
+        self.position[0] += 1 * self.row_coef
+        self.position[1] += 1 * self.col_coef
