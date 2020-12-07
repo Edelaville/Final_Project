c5cd5216009ce7af5972807aa00e45f23332054b
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-12-07 12:58:44 -0500

    Update Piece.py

--- Piece.py
+++ Piece.py
@@ -5,27 +5,28 @@ class pieces:
         for x in piece_types:
             if type == x:
                 self.valid = True
+                piece_types.remove(type)
         self.type = type
         self.position = [0, 0]
         self.row_coef = 0
         self.col_coef = 1
-    
+
     def move(self):
         if self.position[0] == 0 and self.position[1] == 0:
             self.row_coef = 0
             self.col_coef = 1
-            
-        if self.position[0] == 0 and self.position[1] == 9:
+
+        if self.position[0] == 0 and self.position[1] == 10:
             self.row_coef = 1
             self.col_coef = 0
 
-        if self.position[0] == 9 and self.position[1] == 9:
+        if self.position[0] == 10 and self.position[1] == 10:
             self.row_coef = 0
             self.col_coef = -1
-            
-        if self.position[0] == 9 and self.position[1] == 0:
+
+        if self.position[0] == 10 and self.position[1] == 0:
             self.row_coef = -1
             self.col_coef = 0
-        
+
         self.position[0] += 1 * self.row_coef
         self.position[1] += 1 * self.col_coef
