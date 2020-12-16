c3836748cb80a95fa13fcc25f3e00703bc681733
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-12-16 14:01:05 -0500

    Create Location.py

---
+++ Location.py
@@ -0,0 +1,15 @@
+class location:
+
+    def __init__(self, name, type, price):
+        self.name = name
+        self.type = type
+        self.price = price
+
+    def getName(self):
+        return self.name
+
+    def getType(self):
+        return self.type
+
+    def getPrice(self):
+        return self.price
