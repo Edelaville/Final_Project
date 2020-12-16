92ac1f66a1b2a9d9a85d9d1bae2918f6874ca828
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-12-16 14:00:30 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,5 +1,7 @@
 from Piece import pieces
 class Player:
     def __init__(self, piece):
-        input("Which piece would you like?")
+        name = input("Which piece would you like?")
         self.piece = pieces()
+        self.balance = 2000
+        self.properties = []
