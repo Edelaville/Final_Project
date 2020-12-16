89427f0454051b8bd4ef66181516241f193611a6
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-12-16 14:02:56 -0500

    Create Railroads.py

---
+++ Railroads.py
@@ -0,0 +1,27 @@
+from Location import location
+
+
+class Railroads(location):
+
+    def __init__(self, name):
+        super().__init__(name, "Railroad", 200)
+        self.rent = 25
+        self.rentChoices = [25, 50, 100, 200]
+        self.numOfRR = 0
+
+    def getName(self):
+        return self.name
+
+    def getType(self):
+        return self.type
+
+    def getPrice(self):
+        return self.price
+
+    def getRent(self, player):
+        count = 0
+        for x in player.properties:
+            if x.getType() == "Railroad":
+                count += 1
+        self.rent = self.rentChoices[count]
+        return self.rent
