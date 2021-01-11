c2242a7b1761fe4cf6e1e76481f8fceedbe0400b
Edelaville <72936743+Edelaville@users.noreply.github.com>
2021-01-11 09:11:10 -0500

    Update Railroads.py

--- Railroads.py
+++ Railroads.py
@@ -18,7 +18,7 @@ class Railroads(location):
     def getPrice(self):
         return self.price
 
-    def getRent(self, player):
+    def getRent(self):
         count = 0
         for x in player.properties:
             if x.getType() == "Railroad":
