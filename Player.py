62940c882e9a63d8ec3b97d31088d849d9955774
Edelaville <72936743+Edelaville@users.noreply.github.com>
2021-01-11 09:07:43 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,7 +1,29 @@
 from Piece import pieces
+
+
 class Player:
-    def __init__(self, piece):
-        name = input("Which piece would you like?")
-        self.piece = pieces()
-        self.balance = 2000
+    def __init__(self):
+        self.name = input("Please pick a piece from the options above.")
+        self.piece = pieces(self.name)
+        self.balance = 2500
         self.properties = []
+
+    def getName(self):
+        return self.name
+
+    def getBalance(self):
+        return self.balance
+
+    def changeBalance(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def getProperties(self):
+        return self.properties
+
+    def addToList(self, change):
+        self.properties.append(change.getName())
+        return self.properties
+
+    def getPiece(self):
+        return self.piece
