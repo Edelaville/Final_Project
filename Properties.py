334ac90db44322457fdeb5224a0eec2d271b6bcc
Edelaville <72936743+Edelaville@users.noreply.github.com>
2021-01-11 09:08:01 -0500

    Update Properties.py

--- Properties.py
+++ Properties.py
@@ -5,7 +5,7 @@ class Properties(location):
 
     def __init__(self, name, color, price, rent, housecost, onehouse, twohouse, threehouse, fourhouse, hotel):
         super().__init__(name, "Property", price)
-        self.priceChoices = [price, onehouse, twohouse, threehouse, fourhouse, hotel]
+        self.rentChoices = [rent, onehouse, twohouse, threehouse, fourhouse, hotel]
         self.rent = rent
         self.houseHotelCost = housecost
         self.houses = 0
@@ -24,7 +24,7 @@ class Properties(location):
     def getPrice(self):
         return self.price
 
-    def getRent(self):
+    def getRent(self, player):
         return self.rent
 
     def getHouseHotelCost(self):
@@ -35,10 +35,10 @@ class Properties(location):
             print("Set not completed")
             return
         for y in range(6):
-            if self.priceChoices[y] == self.price:
+            if self.rentChoices[y] == self.rent:
                 if x + y > 5:
                     print("Too many houses trying to be placed")
                     return
                 else:
-                    self.price = self.priceChoices[y+x]
+                    self.rent = self.rentChoices[y+x]
                     self.houses += x
